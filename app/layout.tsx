33197f4c8f6ddc72a74e32f0eb50f31011837ea8
Jakub Rejmann <jakub.rejmann@gmail.com>
2023-09-01 15:02:33 +0200

    feat: setup modals provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@components/providers/theme-provider';
 import { cn } from '@lib/utils';
+import { ModalProvider } from '@components/providers/modal-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             defaultTheme="dark"
             enableSystem
             storageKey="discord-theme">
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { ActionTooltip } from '@components/action-tooltip';
+import { useModal } from '@hooks/use-modal-store';
 import { Plus } from 'lucide-react';
 
 export const NavigationAction = () => {
+  const { onOpen } = useModal();
+
   return (
     <div>
-      <button className="group flex items-center">
+      <button className="group flex items-center" onClick={() => onOpen('createServer')}>
         <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
           <ActionTooltip label="New Server" side="right" align="center">
             <Plus className="group-hover:text-white transition text-emerald-500" size={25} />
