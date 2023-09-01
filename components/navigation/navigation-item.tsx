bc9da081f8f5b80f17ee3e4b203176000ef1ddc4
Jakub Rejmann <jakub.rejmann@gmail.com>
2023-09-01 16:25:16 +0200

    feat: server sidebar

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -16,13 +16,11 @@ export const NavigationItem = ({ id, name, imageUrl }: Props) => {
   const params = useParams();
   const router = useRouter();
 
-  const onClick = () => {
-    router.push(`/servers/${id}`);
-  };
-
   return (
     <ActionTooltip label={name} side="right" align="center">
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button
+        onClick={() => router.push(`/servers/${id}`)}
+        className="group relative flex items-center">
         <div
           className={cn(
             'absolute left-0 bg-primary rounded-r-full transition-all w-[4px]',
