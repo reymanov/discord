69986700cbaf9a59554287161aec9fd4081b1125
Jakub Rejmann <jakub.rejmann@gmail.com>
2023-09-11 15:08:39 +0200

    feat: live messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from 'lucide-react';
 import { Fragment } from 'react';
 import { ChatItem } from './chat-item';
 import { format } from 'date-fns';
+import { useChatSocket } from '@hooks/use-chat-socket';
 
 const DATE_FORMAT = 'dd MMM yyyy HH:mm';
 
@@ -40,12 +41,20 @@ export const ChatMessages = ({
   type,
 }: Props) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
     queryKey,
     apiUrl,
     paramKey,
     paramValue,
+  });
+
+  useChatSocket({
+    queryKey,
+    addKey,
+    updateKey,
   });
 
   if (status === 'loading') {
